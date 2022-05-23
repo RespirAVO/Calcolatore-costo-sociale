87cc74d76b14a46cffef86656b84a360b6e78e91
tzyvoski <OxyGen05@outlook.it>
2022-05-23 08:47:27 +0200

    Piccoli miglioramenti

--- src/src/com/marcoschiavello/SocialCostCalculator.java
+++ src/src/com/marcoschiavello/SocialCostCalculator.java
@@ -15,8 +15,9 @@ public class SocialCostCalculator {
 
     public SocialCostCalculator(String recordsFile, LocationRecord pivot, Map<String, Double> costMap) {
         this.totalKM = 0;
-        this.pivot = pivot;
+        this.pivot   = pivot;
         this.costMap = costMap;
+        
         this.readCSVFile(recordsFile);
     }
 
@@ -24,8 +25,8 @@ public class SocialCostCalculator {
         String[] line;
         int[] locationIndexes = new int[2];
         
-        try(CSVReader reader = new CSVReader(new FileReader(file))) {
-            line = reader.readNext();
+        try (CSVReader reader = new CSVReader(new FileReader(file))) {
+            line               = reader.readNext();
             locationIndexes[0] = Arrays.asList(line).indexOf("lat");
             locationIndexes[1] = Arrays.asList(line).indexOf("lon");
 
@@ -35,24 +36,21 @@ public class SocialCostCalculator {
 
                 this.totalKM += this.distanceFromPivot(new LocationRecord(Double.valueOf(line[locationIndexes[0]]), Double.valueOf(line[locationIndexes[1]])));
             }
-        } catch (IOException e) {
-
-        }
+        } catch (IOException e) { }
     }
 
     public double distanceFromPivot(LocationRecord point) {
-        double earthRadius = 6371e3;
-        double lat1Radiant = this.pivot.getLat() * Math.PI / 180;
-        double lat2Radiant = point.getLat() * Math.PI / 180;
+        double earthRadius    = 6371e3;
+        double lat1Radiant    = this.pivot.getLat() * Math.PI / 180;
+        double lat2Radiant    = point.getLat() * Math.PI / 180;
         double latDiffRadiant = (point.getLat() - this.pivot.getLat()) * Math.PI / 180;
         double lonDiffRadiant = (point.getLon() - this.pivot.getLon()) * Math.PI / 180;
 
         double a = Math.sin(latDiffRadiant/2) * Math.sin(latDiffRadiant/2) +
-                  Math.cos(lat1Radiant) * Math.cos(lat2Radiant) *
-                  Math.sin(lonDiffRadiant/2) * Math.sin(lonDiffRadiant/2);
+                   Math.cos(lat1Radiant) * Math.cos(lat2Radiant) *
+                   Math.sin(lonDiffRadiant/2) * Math.sin(lonDiffRadiant/2);
 
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
         double distance = earthRadius * c; // in metri
 
         return distance / 1000;
@@ -61,10 +59,9 @@ public class SocialCostCalculator {
     public double calculateCostOf(String key) {
         if(this.costMap.keySet().contains(key)) {
             return this.costMap.get(key) * this.totalKM;
-        } else {
-            System.out.println("Valore da calcolare non previsto nella mappa");
-            return -1;
-        }
+    
+        System.out.println("Valore da calcolare non previsto nella mappa");
+        return -1;
     }
 
     public void printCost(String key) {
